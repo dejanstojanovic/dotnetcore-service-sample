d998706f8ab67aa73211cbe48021fd47b7e97501
Dejan Stojanovic <d.stojanovic@hotmail.com>
2018-06-14 22:29:58 +0400

    initial impleentation

--- src/Core.Service.Sample/Program.cs
+++ src/Core.Service.Sample/Program.cs
@@ -16,19 +16,19 @@ namespace Core.Service.Sample
             IHost host = new HostBuilder()
                  .ConfigureHostConfiguration(configHost =>
                  {
-                     //configHost.SetBasePath(Directory.GetCurrentDirectory());
-                     //configHost.AddJsonFile("hostsettings.json", optional: true);
-                     //configHost.AddEnvironmentVariables(prefix: "ASPNETCORE_");
-                     //configHost.AddCommandLine(args);
+                     configHost.SetBasePath(Directory.GetCurrentDirectory());
+                     configHost.AddJsonFile("appsettings.json", optional: true);
+                     configHost.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+                     configHost.AddCommandLine(args);
                  })
                  .ConfigureAppConfiguration((hostContext, configApp) =>
                  {
-                     //configApp.AddJsonFile("appsettings.json", optional: true);
-                     //configApp.AddJsonFile(
-                     //    $"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
-                     //    optional: true);
-                     //configApp.AddEnvironmentVariables(prefix: "ASPNETCORE_");
-                     //configApp.AddCommandLine(args);
+                     configApp.AddJsonFile("appsettings.json", optional: true);
+                     configApp.AddJsonFile(
+                         $"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
+                         optional: true);
+                     configApp.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+                     configApp.AddCommandLine(args);
                  })
                 .ConfigureServices((hostContext, services) =>
                 {
@@ -36,8 +36,9 @@ namespace Core.Service.Sample
                 })
                 .ConfigureLogging((hostContext, configLogging) =>
                 {
-                    //configLogging.AddConsole();
-                    //configLogging.AddDebug();
+
+                    configLogging.AddConsole();
+                    configLogging.AddDebug();
                 })
                 .Build();
 
