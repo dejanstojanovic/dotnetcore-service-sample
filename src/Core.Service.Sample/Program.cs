7c8de0d9ed4fab1d131ff1fd744b2562a1a0a8e6
Dejan Stojanovic <d.stojanovic@hotmail.com>
2018-06-14 22:22:52 +0400

    initial impleentation

--- src/Core.Service.Sample/Program.cs
+++ src/Core.Service.Sample/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Core.Service.Sample
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            IHost host = new HostBuilder()
+                 .ConfigureHostConfiguration(configHost =>
+                 {
+                     //configHost.SetBasePath(Directory.GetCurrentDirectory());
+                     //configHost.AddJsonFile("hostsettings.json", optional: true);
+                     //configHost.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+                     //configHost.AddCommandLine(args);
+                 })
+                 .ConfigureAppConfiguration((hostContext, configApp) =>
+                 {
+                     //configApp.AddJsonFile("appsettings.json", optional: true);
+                     //configApp.AddJsonFile(
+                     //    $"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
+                     //    optional: true);
+                     //configApp.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+                     //configApp.AddCommandLine(args);
+                 })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<ServiceHost>();
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    //configLogging.AddConsole();
+                    //configLogging.AddDebug();
+                })
+                .Build();
+
+            await host.RunAsync();
         }
+
+
     }
 }
--- src/Core.Service.Sample/ServiceHost.cs
+++ src/Core.Service.Sample/ServiceHost.cs
@@ -1,10 +1,60 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Core.Service.Sample
 {
-    class ServiceHost: IHostedService
+    public class ServiceHost : IHostedService
     {
+        IApplicationLifetime appLifetime;
+        ILogger<ServiceHost> logger;
+        IHostingEnvironment environment;
+        public ServiceHost(IHostingEnvironment environment,ILogger<ServiceHost> logger, IApplicationLifetime appLifetime)
+        {
+            this.logger = logger;
+            this.appLifetime = appLifetime;
+            this.environment = environment;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            this.appLifetime.ApplicationStarted.Register(OnStarted);
+            this.appLifetime.ApplicationStopping.Register(OnStopping);
+            this.appLifetime.ApplicationStopped.Register(OnStopped);
+
+            return Task.CompletedTask;
+
+        }
+
+        private void OnStarted()
+        {
+            this.logger.LogInformation("OnStarted has been called.");
+
+            // Perform post-startup activities here
+        }
+
+        private void OnStopping()
+        {
+            this.logger.LogInformation("OnStopping has been called.");
+
+            // Perform on-stopping activities here
+        }
+
+        private void OnStopped()
+        {
+            this.logger.LogInformation("OnStopped has been called.");
+
+            // Perform post-stopped activities here
+        }
+
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
     }
 }
